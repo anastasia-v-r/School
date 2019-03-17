1db220d4c8ba4c789afadf78162ff254f20a5506
Faunsce <dominik.rutter@gmail.com>
2019-03-17 04:41:46 -0400

    Finished Program 10 - 2

--- GTC CPP 1/Program 10 - 2/Program 10 - 2/Main.cpp
+++ GTC CPP 1/Program 10 - 2/Program 10 - 2/Main.cpp
@@ -2,68 +2,83 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
-#include <string>
-#include <Windows.h>
+#include <cctype>
+#include "Funcs.h"
+#include "Structs.h"
+
 // Usings
-using str_t = std::string;
-// Structs
-struct speaker
-{
-	str_t name;
-	str_t phone;
-	str_t topic;
-	float fee;
-};
-// Variables
-speaker defSpkr = {
-	"John Doe",
-	"555-555-5555",
-	"Talking",
-	100
-};
-char progress;
-bool end;
-char choice;
-// Vector
-std::vector<speaker> spkrBureau(10, defSpkr);
+using std::cout;
+using std::cin;
+
 //Entry Point
 int main() {
 
+	// Variables
+	std::vector<speaker> spkrBureau(10, defSpkr);
+	char progress = '0';
+	bool end = false;
+	char choice;
+
 	do {
-		std::cout << "Please enter which option you'd like to choose,\n";
-		std::cout << "(A) -";
-		std::cout << "(B) -";
-		std::cout << "(c) -";
-		std::cout << "(F) -";
-		std::cin >> choice;
+		cout << "Please enter which option you'd like to choose,\n";
+		cout << "(A) - Fill entire list\n";
+		cout << "(B) - Edit single element\n";
+		cout << "(C) - Display the entire array\n";
+		cout << "(F) - Exit the program\n";
+		cin >> choice;
+		choice = toupper(choice);
 
 		switch (choice)
 		{
 		case('A'):
-
+			fillVec(spkrBureau);
+			std::cout << "Would you like to continue? [y/n]\n";
+			std::cin >> progress;
+			progress = tolower(progress);
+			break;
 		case('B'):
-
+			std::cout << "Which position would you like to edit in specific?\n";
+			int vec;
+			std::cin >> vec;
+			while (vec < 0 || vec > (spkrBureau.size() - 1)) {
+				std::cout << "That was an invalid element, please try again. This list only contains elements 0 - " << spkrBureau.size() - 1 << "\n";
+				std::cin >> vec;
+			}
+			editVec(spkrBureau, vec);
+			std::cout << "Would you like to continue? [y/n]\n";
+			std::cin >> progress;
+			progress = tolower(progress);
+			break;
 		case('C'):
-
+			readVec(spkrBureau);
+			std::cout << "Would you like to continue? [y/n]\n";
+			std::cin >> progress;
+			progress = tolower(progress);
+			break;
 		case('F'):
 			end = true;
 			break;
+		default:
+			std::cout << "The value entered was invalid, please try again.\n\n";
+			break;
 		}
-		std::cout << "Would you like to progress? [y/n]\n";
-		std::cin >> progress;
+		
 	} while (progress == 'y' || end == true);
 
-	for (int i = 0; i < spkrBureau.size(); ++i) {
-		std::cout << spkrBureau[i].name << spkrBureau[i].phone << spkrBureau[i].topic << spkrBureau[i].fee << "\n";
-	}
-
-	for (int count = 5; count > 0; --count) {
-		std::cout << "Exiting in " << count;
-		Sleep(500);
-		std::cout << ".";
-		Sleep(500);
-		std::cout << ".\n";
-
-	}
+	exit();
 
 }
+
+/*Program 2:
+
+Write a program that keeps track of a speakers’ bureau. The program should use a structure to store the following data about a speaker:
+
+Name
+Telephone Number
+Speaking Topic
+Fee Required
+
+The program should use an array of at least 10 structures. It should let the user enter data into the array, change the contents of any 
+element, and display all the data stored in the array. The program should have a menu-driven user interface.
+
+Input Validation: When the data for a new speaker is entered, be sure the user enters data for all the fields. No negative amounts should be entered for a speaker’s fee.*/
