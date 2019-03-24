ec153a9510fd640405712fe27d7d544b727001d8
Faunsce <dominik.rutter@gmail.com>
2019-03-24 03:19:31 -0400

    Add func prototypes

---
+++ GTC CPP 1/Program 11 - 1/Program 11 - 1/Funcs.h
@@ -0,0 +1,17 @@
+// Includes
+#pragma once
+#include <iostream>
+
+// Protos
+void median(int*, int);
+void mean(int*, int);
+void mode(int*, int);
+void sort(int*, int);
+void printArr(int*, int);
+
+
+
+//In statistics, when a set of values is sorted in ascending or descending order, its median is the middle value.
+//If the set contains an even number of values, the median is the mean, or average, of the two middle values.
+//The mode of a set of values is the value that occurs most often or with the
+//greatest frequency.
