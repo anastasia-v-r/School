b4b076852917b73fca761281b7694c526447586d
Dominik Rutter <dominik.rutter@gmail.com>
2019-03-29 02:05:47 -0400

    Removed unecesarry comment

---
+++ Personal/3D Array/3D Array/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	const int arrSize = 10;
+	int posY = 0, posZ = 0, posX = 0;
+	int arrY[arrSize], arrZ[arrSize], arrX[arrSize];
+
+	for (posY = 0; posY <= arrSize; ++posY)
+	arrY[posY] = 
+	{
+		for (posZ = 0; posZ < arrSize; ++posZ)
+		{
+			for (posX = 0; posX <= arrSize; ++posX)
+			{
+
+			}
+		}
+	}
+}
