46fc843ac2eba7eb0b821a80262f31c88a658f70
Dominik Rutter <dominik.rutter@gmail.com>
2019-04-08 08:54:58 -0400

    customIO update

--- GTC CPP 1/Final Project/Final Project/customIO.cpp
+++ GTC CPP 1/Final Project/Final Project/customIO.cpp
@@ -4,6 +4,7 @@ using std::string;
 #include <iostream>
 using std::cin; using std::cout;
 #include <vector>
+#include "customWait.h"
 
 void smallBox(const string& msg) {
 	for (int i = 0; i < (msg.length() + 4); ++i) {
@@ -31,6 +32,19 @@ void menuScreen(std::vector<string> msg) {
 
 void clearScreen() {
 	for (int i = 0; i < 100; ++i) {
+		cout << "\n";
+	}
+}
+
+void exiting() {
+	wait(1000);
+	cout << "\n[Exiting]\n";
+	for (int i = 3; i > 0; --i) {
+		cout << i;
+		for (int j = 0; j < 3; ++j) {
+			cout << ".";
+			wait(1000 / 3);
+		}
 		cout << "\n";
 	}
 }
--- GTC CPP 1/Final Project/Final Project/customIO.h
+++ GTC CPP 1/Final Project/Final Project/customIO.h
@@ -7,3 +7,4 @@ void smallBox(const string&);
 void upString(string&);
 void menuScreen(std::vector<string>);
 void clearScreen();
+void exiting();
