718bfd8237cf47546c493f6966daa6460fbde449
Dominik Rutter <dominik.rutter@gmail.com>
2019-08-28 04:23:53 -0400

    Finish program 2 - 2

--- GTC CPP 2/Program 2 - 2/main.cpp
+++ GTC CPP 2/Program 2 - 2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	char first[512], middle[512], last[512], all[2048];
+	char first[512], middle[512], last[512], all[2048]{""};
 	
 	std::cout << "Please input your first name \n";
 	std::cin >> first;
@@ -13,6 +13,11 @@ int main() {
 	std::cin >> last;
 	
 	strcat(all, last);
-	std::cout << all;
+	strcat(all, ", ");
+	strcat(all, first);
+	strcat(all, " ");
+	strcat(all, middle);
+	std::cout << all << std::endl;
+	system("pause");
 	std::cin.ignore(1);
 }
