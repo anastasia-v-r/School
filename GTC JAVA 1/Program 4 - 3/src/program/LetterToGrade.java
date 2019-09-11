574f6aaf2c759c4b3afcafee73489c1ba7a4984f
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-10 21:31:18 -0400

    Add Program 4 - 3

---
+++ GTC JAVA 1/Program 4 - 3/src/program/LetterToGrade.java
@@ -0,0 +1,35 @@
+package program;
+
+import java.util.Scanner;
+
+public class LetterToGrade {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+        str = str.toUpperCase();
+        char let = str.charAt(0);
+        int grade;
+        switch (let)
+        {
+            case 'A':
+                grade = 90;
+                break;
+            case 'B':
+                grade = 80;
+                break;
+            case 'C':
+                grade = 75;
+                break;
+            case 'D':
+                grade = 70;
+                break;
+            case 'F':
+                grade = 0;
+                break;
+            default:
+                grade = 1;
+                break;
+        }
+        System.out.println("Letter " + let + " is grade : " + grade);
+    }
+}
