4c5b725b988308f63b2bb448fd68d436bf634cfa
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-17 23:29:01 -0400

    Remove instructions

--- GTC JAVA 1/Program 5 - 1/src/program/ComputeFutureTuition.java
+++ GTC JAVA 1/Program 5 - 1/src/program/ComputeFutureTuition.java
@@ -14,11 +14,3 @@ public class ComputeFutureTuition {
         System.out.println("Cost of 4 years of tuition after year 10 is $" + (df.format(tuition * 4)));
     }
 }
-
-/* 5.7 (Financial application: compute future tuition) Suppose the tuition
-        for a university is $10,000 this year and increases 5% every year. In one
-        year, the tuition will be $10,500. Write a program that displays the tuition in
-        10 years, and the total cost of four years’ worth of tuition after the tenth
-        year.
-
- */
