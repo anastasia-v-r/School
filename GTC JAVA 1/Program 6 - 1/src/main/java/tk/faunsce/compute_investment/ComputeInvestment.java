d03b5ec4cb42e630494bd1b6c50759ec52ca6863
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-21 02:00:36 -0400

    Add ComputeInvestment Program

--- GTC JAVA 1/Program 6 - 1/src/main/java/tk/faunsce/compute_investment/ComputeInvestment.java
+++ GTC JAVA 1/Program 6 - 1/src/main/java/tk/faunsce/compute_investment/ComputeInvestment.java
@@ -1,7 +1,28 @@
 package tk.faunsce.compute_investment;
 
+import java.util.Scanner;
+
 public class ComputeInvestment {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter investment amount :");
+        double principal = input.nextDouble();
+        System.out.println("Enter annual interest rate in decimal (5% == .05) :");
+        double interestRate = input.nextDouble();
+        System.out.println("Enter number of years : ");
+        int years = input.nextInt();
+        System.out.println("Future favlue is :" + futureInvestmentValue(principal, interestRate, years));
+    }
+
+    public static  double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
+        double yield = investmentAmount * (1 + monthlyInterestRate) * (years * 12);
+        return yield;
     }
 }
+
+/*
+futureInvestmentValue = investmentAmount × (1 + monthlyIntrestRate)
+numberOfYears*12
+
+Unclear mathematical function?
+ */
