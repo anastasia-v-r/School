71a0e6daad926cf196f1b46766e4fcaef8a0ae2f
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-24 19:49:57 -0400

    Remove comments

--- GTC JAVA 1/Program 6 - 3/src/main/java/tk/faunsce/polygon_calculator/PolygonCalculator.java
+++ GTC JAVA 1/Program 6 - 3/src/main/java/tk/faunsce/polygon_calculator/PolygonCalculator.java
@@ -17,20 +17,3 @@ public class PolygonCalculator {
         return (n * side * side) / (4 * Math.tan(Math.PI / n));
     }
 }
-
-/*
-6.36 (Geometry: area of a regular polygon) A regular polygon is an n-sided
-polygon in which all sides are of the same length and all angles have
-the same degree (i.e., the polygon is both equilateral and equiangular). The
-formula for computing the area of a regular polygon is
-Area=n×s24×tan (πn)
-Write a method that returns the area of a regular polygon using the
-following header:
-public static double area(int n, double side)
-
-Write a main method that prompts the user to enter the number of sides and
-the side of a regular polygon and displays its area. Here is a sample run:
-Enter the number of sides: 5
-Enter the side: 6.5
-The area of the polygon is 72.69017017488385
- */
