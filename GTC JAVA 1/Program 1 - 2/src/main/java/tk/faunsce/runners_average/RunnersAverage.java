3e54c5f3b864a97af9bde9e136a5f0aa46e196b6
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-26 16:44:55 -0400

    Move to gradle for Program 1 - 2

--- GTC JAVA 1/Program 1 - 2/src/main/java/tk/faunsce/runners_average/RunnersAverage.java
+++ GTC JAVA 1/Program 1 - 2/src/main/java/tk/faunsce/runners_average/RunnersAverage.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package program;
-import java.util.Scanner;
-/**
- *
- * @author drutter5082
- */
+package tk.faunsce.runners_average;
+
 public class RunnersAverage
 {
     public static void main(String[] args)
