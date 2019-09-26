ac82a299ad5468d285e169973105e7ca6e9a9e31
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-26 16:41:25 -0400

    Move to Gradle for Program 1 - 1

---
+++ GTC JAVA 1/Program 1 - 1/src/main/java/tk/faunsce/sequence/Sequence.java
@@ -0,0 +1,10 @@
+package tk.faunsce.sequence;
+
+public class Sequence
+{
+    public static void main(String[] args)
+    {
+        int total = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        System.out.println("Summation of sequence is ... " + total);
+    }
+}
