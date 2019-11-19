69d93f5e19c8913262d514f1e8130a66f8737008
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-18 23:52:35 -0500

    init program 11 - 1

---
+++ GTC CPP 2/Program 11 - 1/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void hasLetter(const std::string& /* string */, const char& /* letter */);
+
+int main() {
+	
+}
+
+void hasLetter(const std::string& string, const char& letter) {
+}
