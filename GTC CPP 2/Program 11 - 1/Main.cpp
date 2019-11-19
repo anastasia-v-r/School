9f71dda082c2b4eed772fdec445fca7993526a48
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-19 00:36:08 -0500

    Finish Program 11 - 1

--- GTC CPP 2/Program 11 - 1/Main.cpp
+++ GTC CPP 2/Program 11 - 1/Main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <string>
 
-void hasLetter(const std::string& /* string */, const char& /* letter */);
+bool hasLetter(const std::string& /* string */, const char& /* letter */);
 
 int main() {
-	
+	std::string test;
+	char letter;
+	std::cout << "Please enter a string to test" << std::endl;
+	std::cout << ">> " << std::flush;
+	std::getline(std::cin, test);
+	std::cout << "Please enter a single character for the string to be checked for" << std::endl;
+	std::cout << ">> " << std::flush;
+	std::cin >> letter;
+	std::cout << "String [" << test << (hasLetter(test, letter) ? "] contains ['" : "] does not contain ['") << (char)std::toupper(letter) << "'/'" << (char)std::tolower(letter) << "']" << std::endl;
 }
 
-void hasLetter(const std::string& string, const char& letter) {
+bool hasLetter(const std::string& string, const char& letter) {
+	static unsigned int position = 0;
+	if (std::tolower(string[position]) == std::tolower(letter)) {
+		return true;
+	} else {
+		if (position < string.size() - 1) {
+			position++;
+			return hasLetter(string, letter);
+		} else {
+			return false;
+		}
+	}
 }
