04c1ec08f1ac7d2a4c240e183d7dc4b8876ab86a
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-20 07:51:25 -0500

    finish parking meter class

--- GTC CPP 2/Final/ParkingMeter.cpp
+++
@@ -1,2 +0,0 @@
-#include "ParkingMeter.hpp"
-
--- GTC CPP 2/Final/ParkingMeter.hpp
+++ GTC CPP 2/Final/ParkingMeter.hpp
@@ -2,5 +2,9 @@
 
 class ParkingMeter
 {
-
+public:
+	inline ParkingMeter(double minutes) { time_remaining = minutes; };
+	inline bool passTime(double time) { time_remaining -= time; return (time_remaining < 0); };
+private:
+	double time_remaining;
 };
