187d5b5e5a0b6e47563e93be943b53d45b944ecd
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-20 18:42:47 -0500

    allow maps for id check

--- GTC CPP 2/Final/Funcs.cpp
+++ GTC CPP 2/Final/Funcs.cpp
@@ -5,3 +5,8 @@ unsigned short getFreshId(const std::set<unsigned short>& knownIds) {
 	auto id = randNum<unsigned short>();
 	return (knownIds.count(id)) ? getFreshId(knownIds) : id;
 }
+
+unsigned short getFreshId(const std::map<unsigned short, std::string>* knownIds) {
+	auto id = randNum<unsigned short>();
+	return (knownIds->count(id)) ? getFreshId(knownIds) : id;
+}
--- GTC CPP 2/Final/Funcs.hpp
+++ GTC CPP 2/Final/Funcs.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <set>
+#include <map>
 #include "RandomNumber.tpp"
 
 unsigned short getFreshId(const std::set<unsigned short>& /* Known Ids */);
+unsigned short getFreshId(const std::map<unsigned short, std::string>& /* Known Ids */);
