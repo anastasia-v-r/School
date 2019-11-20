dd90de73baed2a1ca5354ab4c0161cf4b17a0717
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-20 07:53:19 -0500

    delete default constructor

--- GTC CPP 2/Final/ParkingMeter.hpp
+++ GTC CPP 2/Final/ParkingMeter.hpp
@@ -3,6 +3,7 @@
 class ParkingMeter
 {
 public:
+	ParkingMeter() = delete;
 	inline ParkingMeter(double minutes) { time_remaining = minutes; };
 	inline bool passTime(double time) { time_remaining -= time; return (time_remaining < 0); };
 private:
