6d708330f00aed62911e3d96f242d8873eb9b438
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-21 02:58:17 -0500

    populate street using user input

--- GTC CPP 2/Final/Main.cpp
+++ GTC CPP 2/Final/Main.cpp
@@ -10,18 +10,39 @@
 
 int main() {
 	try {
+		// Data
 		double city_revenue{ 0.0 };
 		double max_time{ 180.0 };
-		unsigned int parking_space{ 1u };
-		 
+		unsigned int parking_spaces{ 1u };
+		unsigned int parking_freq{ 50u };
+		unsigned int passTime{ 1u };
 
+		// Get officer name and create officer
+		Custom::output("Please enter the name of the officer in the format of \"first last\"");
+		PoliceOfficer officer(Custom::input<std::string>('\n'));
+		
+		// Get parking space amount
+		Custom::output("Please enter the number of parking spaces that exist within the street [Max 1k]");
+		parking_spaces = Custom::input<unsigned int>(1u, 1000u);
 
-		PoliceOfficer officer("Andrew");
+		// Get parking frequency
+		Custom::output("Please enter the parking frequency [0-100] (not guaranteed)");
+		parking_freq = Custom::input<unsigned int>(1u, 100u);
+
+		// Setup random cars
 		std::vector<std::pair<ParkingMeter, std::shared_ptr<ParkedCar>>> streetSide;
-		streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, max_time)), std::make_shared<ParkedCar>(MAKE_MODEL::TOYOTA_CAMRY, COLOR::SILVER)));
-		streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, max_time)), nullptr));
-		streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, max_time)), std::make_shared<ParkedCar>(MAKE_MODEL::HONDA_ACCORD, COLOR::YELLOW)));
-		streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, max_time)), std::make_shared<ParkedCar>(MAKE_MODEL::DODGE_CHARGER, COLOR::GREEN)));
+		for (int i = 0; i < parking_spaces; i++) {
+			if (Custom::randNum<unsigned int>(0u, 100u) < parking_freq) {
+				std::shared_ptr<ParkedCar> tempCar = std::make_shared<ParkedCar>(
+					static_cast<MAKE_MODEL>(Custom::randNum<unsigned int>(0u, static_cast<unsigned int>(MAKE_MODEL::DODGE_CHARGER))),
+					static_cast<COLOR>(Custom::randNum<unsigned int>(0u, static_cast<unsigned int>(COLOR::BLACK)))
+				);
+				streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, 180.0)), tempCar));
+			} else {
+				streetSide.emplace_back(std::make_pair(ParkingMeter(Custom::randNum<double>(0.0, 180.0)), nullptr));
+			}
+		}
+
 		for (auto& parking_space : streetSide) {
 			if (parking_space.second != nullptr) {
 				parking_space.second->passTime(Custom::randNum<float>(0.0, max_time * 2.0));
